eea408d54863e9f0d0055461aee14a6c27757e1a
Lukas <93530655+LukasJansen100@users.noreply.github.com>
2022-12-15 16:03:30 +0100

    Delete App.test.js

--- src/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from "../App";
-
-test('renders all menu items title', () => {
-  render(<App />);
-  const linkElement = screen.getByText("All Menu Items");
-  expect(linkElement).toBeInTheDocument();
-});
