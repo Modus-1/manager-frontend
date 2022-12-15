2fdb94c3d26ffab5c87043b1d63d58ea6058f507
Lukas Jansen <I477609@fhict.nl>
2022-12-15 11:25:00 +0100

    pulled everything out of folder

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import SideBar from "../src/components/sidebar/sidebar";
+import AddMenuItemForm from "../src/components/addIngredientForm/addIngredientForm";
+
+import { ReactDOM } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar></SideBar>
+
+      
+    </div>
+  );
+}
+
+export default App;
