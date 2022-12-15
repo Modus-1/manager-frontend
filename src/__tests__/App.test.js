50fa8ea6e4b6c9debb2f29055027c88b57adcba6
Judah <i460958@fhict.nl>
2022-12-15 15:18:11 +0100

    Changed test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "../App";
 
-test('renders learn react link', () => {
+test('renders all menu items title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("All Menu Items");
   expect(linkElement).toBeInTheDocument();
 });
