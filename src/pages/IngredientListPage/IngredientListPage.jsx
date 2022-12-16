5937788cdf1cd8bfe28cb4e8e84e2856fa6849e4
Lukas Jansen <I477609@fhict.nl>
2022-12-16 14:32:16 +0100

    Added ingredient list page

--- src/pages/IngredientListPage/IngredientListPage.jsx
+++ src/pages/IngredientListPage/IngredientListPage.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import {
+  getAllIngredients,
+  getIngredientByID,
+} from "../../services/MenuService";
+import "./IngredientListPage.css";
+
 export default function IngredientListPage() {
-    return(
-        <div>
-            <p>ingredient list page</p>
-        </div>
-    )
+  const [Ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    const fetchAllIngredients = async () => {
+      const data = await getAllIngredients();
+      return data;
+    };
+    fetchAllIngredients().then((res) => {
+      setIngredients(res);
+    });
+  }, []);
+
+  return (
+    <table class="ingredient-list-container">
+      <tr class="row-header">
+        <th>Ingredient Name</th>
+        <th>Amount in stock</th>
+        <th>Amount in weight (kg)</th>
+        <th>Allergens</th>
+      </tr>
+      {Ingredients.map((ingredient) => (
+        <tr class="ingredient-row" key={ingredient.id}>
+          <td>{ingredient.name}</td>
+          <td>{ingredient.stock}</td>
+          <td>{ingredient.weight}</td>
+          <td>{ingredient.allergens}</td>
+        </tr>
+      ))}
+    </table>
+  );
 }
